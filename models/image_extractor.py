7d198272a6196fbc73d8ad9e6cdcbefd5b672e85
HongHanh <honghanh.nguyen2104@gmail.com>
2020-10-12 21:39:15 +0700

    update image_extractor and config

--- models/image_extractor.py
+++ models/image_extractor.py
@@ -5,17 +5,27 @@ import torch.nn.functional as F
 from tqdm import tqdm
 
 class ImageClassifier(nn.Module):
+    arch = {
+        'resnet18': models.resnet18,
+        'resnet34': models.resnet34,
+        'resnet50': models.resnet50,
+        'resnet101': models.resnet101,
+        'resnet152': models.resnet152,
+        'wide_resnet50_2': models.wide_resnet50_2,
+        'wide_resnet101_2': models.wide_resnet101_2,
+    }
     def __init__(self,
+                 version,
                  nclasses):
         super().__init__()
-        cnn = models.resnet50(pretrained=True)
+        cnn = ImageClassifier.arch[version](pretrained=True)
         self.feature_dim = cnn.fc.in_features
         self.model = nn.Sequential(*list(cnn.children())[:-1])
         self.classifier = nn.Linear(self.feature_dim, nclasses)
 
     def forward(self, x):
         #print(x.shape)
-        features = self.model(x) # [B, D, H', W']
+        features = self.model(x)
         #print(features.shape)
         out = features.view(-1, self.feature_dim)
         #print(out.shape)
@@ -24,7 +34,7 @@ class ImageClassifier(nn.Module):
         return out
 
 def main():
-    model = ImageClassifier(3)
+    model = ImageClassifier('resnet50', 3)
     dev_id = 'cuda:0' if torch.cuda.is_available() else 'cpu'
     device = torch.device(dev_id)
     model = model.to(device)
